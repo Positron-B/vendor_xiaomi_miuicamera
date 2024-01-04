d56583ac3907c3e34302e7365461e91b2ecb2eb7
Alexander Winkowski <dereference23@outlook.com>
2024-01-04 06:17:24 +0000

    Add libgui shim
    Co-authored-by: Arian <arian.kulmer@web.de>

---
+++ shims/libgui_shim_miuicamera.c
@@ -0,0 +1,9 @@
+/*
+ * Copyright (C) 2023-2024 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+void _ZN7android18BnProducerListener16onBufferDetachedEi(int slot) {
+   (void)slot;
+}
