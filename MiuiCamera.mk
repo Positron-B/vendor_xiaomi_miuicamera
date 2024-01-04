8f29b9dfd993066000e25b0f82bba7d16a814bda
Alexander Winkowski <dereference23@outlook.com>
2024-01-04 07:23:20 +0000

    Move to /system_ext
    Native libraries in /product must be unbundled from the other
    partitions. Move the components to /system_ext to simplify
    providing dependencies.

--- MiuiCamera.mk
+++ MiuiCamera.mk
@@ -14,9 +14,10 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     MiuiExtraPhoto
 
 # Properties
-PRODUCT_PRODUCT_PROPERTIES += \
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
     persist.vendor.camera.privapp.list=com.android.camera \
+    ro.com.google.lens.oem_camera_package=com.android.camera \
     ro.miui.build.region=in \
     ro.miui.notch=1 \
     ro.product.mod_device=_global \
-    ro.com.google.lens.oem_camera_package=com.android.camera
+    vendor.camera.aux.packagelist=com.android.camera
